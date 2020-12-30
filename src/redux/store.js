c8c43c22616cbb929e31d0a3a47e30c62be69b39
guswls1846 <guswls1846@naver.com>
2020-12-30 16:43:48 +0900

    [12.30]

--- src/redux/store.js
+++ src/redux/store.js
@@ -3,7 +3,7 @@ import { createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
-import reducers from "./reducer";
+import reducers from "./reducers/reducer";
 
 const persistConfig = {
   key: "root",
